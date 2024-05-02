b93003f0d0ab3cb141274d8e40818eec0897e205
programmer-SS <167070988+programmer-SS@users.noreply.github.com>
2024-05-02 18:24:19 +0900

    階乗関数の実装２

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 /**
  * 与えられた自然数の階乗を返す
  * 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
- * @param {Number} n
- * @returns {Number}
+ * @param {number} n
+ * @returns {number}
  */
 function factorial(n) {
   let result = 1;
@@ -17,4 +17,4 @@ assert.strictEqual(factorial(1), 1, `1の階乗は1ですが、実際は${factor
 assert.strictEqual(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
 assert.strictEqual(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
 assert.strictEqual(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
-console.log('すべてのテストを通過しました');
+console.log('すべてのテストを通過しました');  
